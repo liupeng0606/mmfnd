e859ca0a3e7ac4bd2970ed78eb53c4a2460cc039
liupeng <784606255@qq.com>
2023-02-14 22:41:11 +0800

    Update train.py
    init

--- train.py
+++ train.py
@@ -32,7 +32,6 @@ def get_args(parser):
     parser.add_argument("--embed_sz", type=int, default=300)
     parser.add_argument("--freeze_img", type=int, default=0)
     parser.add_argument("--freeze_txt", type=int, default=0)
-    parser.add_argument("--glove_path", type=str, default="/path/to/glove_embeds/glove.840B.300d.txt")
     parser.add_argument("--gradient_accumulation_steps", type=int, default=24)
     parser.add_argument("--hidden", nargs="*", type=int, default=[])
     parser.add_argument("--hidden_sz", type=int, default=768)
@@ -44,15 +43,15 @@ def get_args(parser):
     parser.add_argument("--lr_patience", type=int, default=2)
     parser.add_argument("--max_epochs", type=int, default=100)
     parser.add_argument("--max_seq_len", type=int, default=512)
-    parser.add_argument("--model", type=str, default="bow", choices=["bow", "img", "bert", "concatbow", "concatbert", "mmbt", "mmbt_drop"])
+    parser.add_argument("--model", type=str, default="mmbt_drop")
     parser.add_argument("--n_workers", type=int, default=12)
     parser.add_argument("--name", type=str, default="nameless")
     parser.add_argument("--num_image_embeds", type=int, default=1)
     parser.add_argument("--patience", type=int, default=10)
     parser.add_argument("--savedir", type=str, default="/path/to/save_dir/")
     parser.add_argument("--seed", type=int, default=123)
-    parser.add_argument("--task", type=str, default="fakeddit", choices=["weibo", "vsnli", "food101", "fakeddit"])
-    parser.add_argument("--task_type", type=str, default="classification", choices=["multilabel", "classification"])
+    parser.add_argument("--task", type=str, default="fakeddit")
+    parser.add_argument("--task_type", type=str, default="classification")
     parser.add_argument("--warmup", type=float, default=0.1)
     parser.add_argument("--weight_classes", type=int, default=1)
 
@@ -186,31 +185,15 @@ def model_forward(i_epoch, model, args, criterion, batch):
     freeze_img = i_epoch < args.freeze_img
     freeze_txt = i_epoch < args.freeze_txt
 
-    if args.model == "bow":
-        txt = txt.cuda()
-        out = model(txt)
-    elif args.model == "img":
-        img = img.cuda()
-        out = model(img)
-    elif args.model == "concatbow":
-        txt, img = txt.cuda(), img.cuda()
-        out = model(txt, img)
-    elif args.model == "bert":
-        txt, mask, segment = txt.cuda(), mask.cuda(), segment.cuda()
-        out = model(txt, mask, segment)
-    elif args.model == "concatbert":
-        txt, img = txt.cuda(), img.cuda()
-        mask, segment = mask.cuda(), segment.cuda()
-        out = model(txt, mask, segment, img)
-    else:
-        assert args.model == "mmbt_drop"
-        for param in model.enc.img_encoder.parameters():
-            param.requires_grad = not freeze_img
-        for param in model.enc.encoder.parameters():
-            param.requires_grad = not freeze_txt
+    
+    assert args.model == "mmbt_drop"
+    for param in model.enc.img_encoder.parameters():
+        param.requires_grad = not freeze_img
+    for param in model.enc.encoder.parameters():
+        param.requires_grad = not freeze_txt
 
-        txt, img = txt.cuda(), img.cuda()
-        mask, segment = mask.cuda(), segment.cuda()
+    txt, img = txt.cuda(), img.cuda()
+    mask, segment = mask.cuda(), segment.cuda()
         
         # if model.training:
         #     tgt = torch.cat([tgt,tgt], dim=-1).cuda()
@@ -219,12 +202,12 @@ def model_forward(i_epoch, model, args, criterion, batch):
         #     return ce_loss, out1, tgt
 
         
-        out1, fts1 = model(txt, mask, segment, img)
-        tgt = tgt.cuda()
+    out1, fts1 = model(txt, mask, segment, img)
+    tgt = tgt.cuda()
     
 
-        ce_loss = criterion(out1, tgt)
-        return ce_loss, out1, tgt
+    ce_loss = criterion(out1, tgt)
+    return ce_loss, out1, tgt
 
 
 def train(args):
